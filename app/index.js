b7cf510c7df238e309e5d1ca17d26e8f33aafa53
Mauricio <mauhmg@gmail.com>
2022-11-22 21:03:09 -0300

    Instalação do bootstrap // Configurações do projeto

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const db = require('./models/db.js');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.render('index');
 });
 
 app.listen(8080, () => {
---
+++ app/models/db.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const banco = 'guia_press';
+const db = new Sequelize(banco, 'root', 'root', {
+	host: 'localhost',
+	dialect: 'mysql'
+});
+
+db.authenticate().then(() => {
+	console.log(`Banco de de dados [${banco}] conectado com sucesso!`);
+}).catch((err)=>{
+	console.log(err);
+});
+
+module.exports = db;
