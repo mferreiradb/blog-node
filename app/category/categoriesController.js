0593d585bec6f51a817c3a4408125d83f5f6a34c
Mauricio <mauhmg@gmail.com>
2022-11-24 21:23:40 -0300

    Adição de navbar e criação de arquivo para o formulário de categorias

--- app/category/categoriesController.js
+++ app/category/categoriesController.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/categories', (req, res) => {
-	res.send('Rota de categorias');
+router.get('/admin/categories/new', (req, res) => {
+	res.render('admin/categories/new.ejs');
 });
 
 module.exports = router;
