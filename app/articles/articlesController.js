e0c0d6262b53e9c5939e105286798d31bceafb77
Mauricio <mauhmg@gmail.com>
2022-11-29 22:52:48 -0300

    Passagem dos dados do bancop de dados para o front de cadastro de artigo

--- app/articles/articlesController.js
+++ app/articles/articlesController.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Category = require ('../models/Category');
 
 router.get('/articles', (req, res) => {
-	res.render('admin/articles/new');
+	Category.findAll().then((categories) => {
+		res.render('admin/articles/new', {categories: categories});
+	});
+});
+
+router.post('/articles/save', () => {
+  
 });
 
 module.exports = router;
