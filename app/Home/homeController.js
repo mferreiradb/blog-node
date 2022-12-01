6593cb81164df0249bc258da6e1379c2e4fa6899
Mauricio <mauhmg@gmail.com>
2022-12-01 20:45:17 -0300

    Criação da rota de leitura dos artigos // criação d apágina para leitura dos artigos

--- app/Home/homeController.js
+++ app/Home/homeController.js
@@ -11,4 +11,22 @@ router.get('/', (req, res) => {
 	});
 });
 
+router.get('/:slug', (req, res) => {
+	let slug = req.params.slug;
+	Article.findOne({
+		where: {
+			slug: slug
+		}
+	}).then((article) => {
+		if (article != undefined) {
+			res.render('article', {article: article});
+		} else {
+			res.redirect('/');
+		}
+	}).catch((err) => {
+		res.redirect('/');
+		console.log(err);
+	});
+});
+
 module.exports = router;
