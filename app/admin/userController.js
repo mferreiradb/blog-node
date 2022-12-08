d02ed55f2ede46bf89c8bb04624dd83a8597e3ee
Mauricio <mauhmg@gmail.com>
2022-12-07 22:50:50 -0300

    Adição do hash de senha à rota post do cadastro de usuario

--- app/admin/userController.js
+++ app/admin/userController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
 	res.send('Lista de usuários');
@@ -12,11 +13,16 @@ router.get('/admin/users/create', (req, res) => {
 
 router.post('/admin/user/create', (req, res) => {
 	let {email, password} = req.body;
+	let salt = bcrypt.genSaltSync(10);
+	let hash = bcrypt.hashSync(password, salt);
+
 	User.create({
 		email: email,
-		password: password
+		password: hash
 	}).then(() => {
 		res.redirect('/admin/users');
+	}).catch((err) => {
+		console.log(err);
 	});
 });
 
